dbd8453eaedfaa122b1908169f2b0e1f5a4e809f
Javiertries2code <superjabo@live.com>
2024-10-01 20:38:18 +0200

    vertical layout fully working, pending to do the vertical

--- Calculadora/composeCalcu/app/src/main/java/com/example/composecalcu/MainActivity.kt
+++ Calculadora/composeCalcu/app/src/main/java/com/example/composecalcu/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.composecalcu
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,13 +11,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import java.math.*
+
+
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -38,21 +45,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-@Composable
-fun Display_Calcu () {
 
-Row(modifier = Modifier.fillMaxWidth().padding(10.dp).height(120.dp)){
-    TextField(
-        value = "0",
-        onValueChange = {},
-        modifier = Modifier.fillMaxWidth(),
-        textStyle = TextStyle(
-            fontSize = 75.sp,
-            color = Color.Black,
-            textAlign = TextAlign.End
-        ),
-    )
-}}
 
 @Composable
 fun OperatorButton(
@@ -75,8 +68,8 @@ fun OperatorButton(
 
 @Composable
 fun NumberButton(
-    number: Int,
-    onNumberClick: (Int) -> Unit,
+    number: String,
+    onNumberClick: (String) -> Unit,
     backgroundColor: Color = colorResource(R.color.focused_blue)
 
 ) {
@@ -90,61 +83,110 @@ fun NumberButton(
     }
 }
 
-@Composable
-fun LineaNum( number1: Int, number2: Int,number3: Int, operator: String   ) {
-
-
-        Row(
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            NumberButton(number = number1, onNumberClick = ::NumberButtonClick)
-            NumberButton(number = number2, onNumberClick = ::NumberButtonClick)
-            NumberButton(number = number3, onNumberClick = ::NumberButtonClick)
-            OperatorButton(operator = operator, onOperatorClick = ::OperatorButtonClick)
-        }
-
-}
-
-@Composable
-fun LineaOp( operator: String, operator1: String, operator2: String, operator3: String,  ) {
-
-
-    Row(
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        OperatorButton(operator = operator, onOperatorClick = ::OperatorButtonClick)
-        OperatorButton(operator = operator1, onOperatorClick = ::OperatorButtonClick)
-        OperatorButton(operator = operator2, onOperatorClick = ::OperatorButtonClick)
-        OperatorButton(operator = operator3, onOperatorClick = ::OperatorButtonClick)
-    }
-
-}
 
 
 
 @Preview(showBackground = true)
 @Composable
 fun my_calcu() {
+    var numberDisplay by rememberSaveable { mutableStateOf("0") }
+ var sign: String
+    fun OperatorButtonClick(operator: String) {
+        if (operator == "C")
+            numberDisplay = "0"
+        else if (numberDisplay == "0")
+            numberDisplay=operator
+        else if(operator == "+/-")
+        {
+            if (numberDisplay[0] != '-') {
+                sign = "-" + numberDisplay
+                numberDisplay = sign
+            }
+            else {
+                sign = numberDisplay.substring(1)
+                numberDisplay = sign
+            }
+        }
+        else
+            numberDisplay+=operator
 
+    }
+    fun NumberButtonClick(number: String) {
+        if (numberDisplay == "0")
+            numberDisplay=number
+        else
+            numberDisplay+=number
+    }
   Column(modifier = Modifier
       .fillMaxSize()
       .background(Color.DarkGray),
       verticalArrangement = Arrangement.SpaceEvenly
   )
   {
-      Display_Calcu()
-      LineaOp("C", "%","/", "X")
-      LineaNum(7,8,9, "+")
-      LineaNum(4,5,6, "-")
-      LineaNum(1,2,3, "/")
+
+      Row(modifier = Modifier
+          .fillMaxWidth()
+          .padding(10.dp)
+          .height(120.dp)){
+          TextField(
+              value = numberDisplay,
+              onValueChange = {},
+              modifier = Modifier.fillMaxWidth(),
+              textStyle = TextStyle(
+                  fontSize = 75.sp,
+                  color = Color.Black,
+                  textAlign = TextAlign.End
+              ),
+          )
+      }
+      //LineaOp("C", "%","/", "X")
+      Row( horizontalArrangement = Arrangement.SpaceEvenly,
+          modifier = Modifier.fillMaxWidth())
+      {
+          OperatorButton(operator = "C", onOperatorClick = ::OperatorButtonClick)
+          OperatorButton(operator = "%", onOperatorClick = ::OperatorButtonClick)
+          OperatorButton(operator = "/", onOperatorClick = ::OperatorButtonClick)
+          OperatorButton(operator = "X", onOperatorClick = ::OperatorButtonClick)
+
+      }
+
+   //   LineaNum(7,8,9, "+")
+      Row( horizontalArrangement = Arrangement.SpaceEvenly,
+          modifier = Modifier.fillMaxWidth())
+      {
+          NumberButton(number = "7", onNumberClick = ::NumberButtonClick)
+          NumberButton(number = "8", onNumberClick = ::NumberButtonClick)
+          NumberButton(number = "9", onNumberClick = ::NumberButtonClick)
+          OperatorButton(operator = "+", onOperatorClick = ::OperatorButtonClick)
+
+      }
+
+     // LineaNum(4,5,6, "-")
+      Row( horizontalArrangement = Arrangement.SpaceEvenly,
+          modifier = Modifier.fillMaxWidth())
+      {
+          NumberButton(number = "4", onNumberClick = ::NumberButtonClick)
+          NumberButton(number = "5", onNumberClick = ::NumberButtonClick)
+          NumberButton(number = "6", onNumberClick = ::NumberButtonClick)
+          OperatorButton(operator = "-", onOperatorClick = ::OperatorButtonClick)
+
+      }
+     // LineaNum(1,2,3, "/")
+      Row( horizontalArrangement = Arrangement.SpaceEvenly,
+          modifier = Modifier.fillMaxWidth())
+      {
+          NumberButton(number = "1", onNumberClick = ::NumberButtonClick)
+          NumberButton(number = "2", onNumberClick = ::NumberButtonClick)
+          NumberButton(number = "3", onNumberClick = ::NumberButtonClick)
+          OperatorButton(operator = "/", onOperatorClick = ::OperatorButtonClick)
+
+      }
 
      Row( horizontalArrangement = Arrangement.SpaceEvenly,
          modifier = Modifier.fillMaxWidth())
      {
-         NumberButton(number = 0, onNumberClick = ::NumberButtonClick)
-         OperatorButton(operator = ".", onOperatorClick = ::OperatorButtonClick)
+         NumberButton(number = "0", onNumberClick = ::NumberButtonClick)
+         NumberButton(number = ".", onNumberClick = ::NumberButtonClick)
          OperatorButton(operator = "+/-", onOperatorClick = ::OperatorButtonClick)
          OperatorButton(operator = "=", onOperatorClick = ::OperatorButtonClick)
 
@@ -154,10 +196,4 @@ fun my_calcu() {
 
 }
 
-fun OperatorButtonClick(operator: String) {
-    println(" $operator")
-}
 
-fun NumberButtonClick(number: Int) {
-    println(" $number")
-}
